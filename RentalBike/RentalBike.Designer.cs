5dc156a2ba93f3b4c7c32e4acc9e051af4446f30
jatinder303 <Jatinder303@gmail.com>
2022-09-17 22:07:02 +1200

    updates

--- RentalBike/RentalBike.Designer.cs
+++ RentalBike/RentalBike.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\em8233\source\repos\RentalBike\RentalBike\RentalBike.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\jatin\Source\Repos\RentalBike\RentalBike\RentalBike.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
